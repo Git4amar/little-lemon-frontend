b37b500333698f50ca5f7c6e717dc2c06a553109
Amarpreet S. Bir <apsbir@icloud.com>
2023-09-29 01:39:04 -0700

    added text color change with scroll animation for about section -> todo: reduce scrolling speed during the motion

--- src/sections/About/AboutOwnersText.js
+++ src/sections/About/AboutOwnersText.js
@@ -5,23 +5,51 @@ import { useRef, useEffect } from "react";
 const AboutOwnersText = ({ children }) => {
 
     const scope = useRef(null);
-    const { scrollYProgress } = useScroll({
+    const { scrollYProgress, scrollY } = useScroll({
         target: scope,
-        offset: ["center end", "center"]
+        offset: ["center end", "0.25 center"]
     })
 
     const words = children.split(" ");
     let wordSpanElems = useRef(null);
     let wordSpanElemsL = useRef(null);
     useEffect(() => {
-        wordSpanElems.current = scope.current.querySelectorAll("span");
+        wordSpanElems.current = Array.from(scope.current.querySelectorAll("span"));
         wordSpanElemsL.current = wordSpanElems.current.length;
     }, [])
 
+    let i = 0;
+    let coloredWordSpans = []
     useAnimationFrame(() => {
-        if (scrollYProgress.current > 0 && scrollYProgress.current < 1) {
-            let wordElem = wordSpanElems.current[Math.trunc((wordSpanElemsL.current) * scrollYProgress.current)];
-            wordElem.style.cssText = `color: #333333;`
+        if (scrollYProgress.current * 100 > 0 && scrollYProgress.current * 100 < 100) {
+            if (scrollY.current > scrollY.prev && scrollYProgress.current * 100 > i * 100 / wordSpanElemsL.current && i < wordSpanElemsL.current) {
+                wordSpanElems.current[0].style.cssText = "color: #333333;";
+                coloredWordSpans.unshift(wordSpanElems.current[0]);
+                wordSpanElems.current.shift();
+                i++;
+            }
+            else if (scrollY.current < scrollY.prev && scrollYProgress.current * 100 < i * 100 / wordSpanElemsL.current && i >= 0) {
+                coloredWordSpans[0].style.cssText = "color: #F4CE14;";
+                wordSpanElems.current.unshift(coloredWordSpans[0]);
+                coloredWordSpans.shift();
+                i--;
+            }
+        }
+        else if (scrollYProgress.current >= 1 && wordSpanElems.current.length > 0) {
+            i = wordSpanElemsL.current;
+            for (let word of wordSpanElems.current) {
+                word.style.cssText = "color: #333333;";
+                coloredWordSpans.unshift(word);
+            }
+            wordSpanElems.current = [];
+        }
+        else if (scrollYProgress.current <= 0 && coloredWordSpans.length > 0) {
+            i = 0;
+            for (let word of coloredWordSpans) {
+                word.style.cssText = "color: #F4CE14;";
+                wordSpanElems.current.unshift(word);
+            }
+            coloredWordSpans = [];
         }
     })
 
@@ -42,6 +70,7 @@ const AboutOwnersText = ({ children }) => {
             >
                 {words.map((word, index) => <span
                     key={word + "-" + index}
+                    data-word-indicator={word + "-" + index}
                 >
                     {word + " "}
                 </span>)}
