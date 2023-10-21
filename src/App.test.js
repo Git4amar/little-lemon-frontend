d4d0d27876ab5afac33a430a18bc9035f0040677
Amarpreet S. Bir <apsbir@icloud.com>
2023-10-21 01:00:13 -0700

    partial test setup needs further corrections and work -> todo: also try with Selenium

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import { render, screen } from '@testing-library/react';
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+import { act } from "@testing-library/react";
 
-test('renders learn react link', () => {
+jest.mock('@studio-freight/lenis');
+jest.mock('./components/Navigation/NavItem');
+
+test('renders little lemon title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByText(/little lemon/i);
+  expect(title).toBeInTheDocument();
 });
+
+// test('renders table reservation formtitle', () => {
+test('renders table reservation buttons', () => {
+  render(<App />);
+  const reservationBtns = screen.getAllByRole('button', { name: /reserve your table/i });
+  expect(reservationBtns.length).toBe(2);
+  // userEvent.click(reservationBtns[0]);
+})
